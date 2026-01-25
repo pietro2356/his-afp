688241afe3113fcfcb301bf7dcec7fefbe9a8d6b
YRNMAMA <mamadou.gningue@marconirovereto.it>
2026-01-25 16:17:45 +0100

    setup corretto

---
+++ afp-his/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [providePrimeNG({
+    theme: {
+      preset: Aura, // Tema di default PrimeNG
+      options: {
+        ripple: true, // Animazioni
+        darkModeSelector: '.my-app-dark' // Tema scuro
+      }
+    }
+  }),
+  
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
+  ]
+};
