eb48a409ae91c85c2cec363daa63d224a311b1a0
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-02-04 21:17:17 +0100

    feat: ✨ update card component to use input for paziente
    Refactor the CardPz component to utilize Angular's input binding for the paziente property instead of a signal. This change enhances the component's flexibility by allowing it to receive patient data dynamically. Additionally, the elenco-pz component has been updated to render a list of patients using the new input structure, improving the overall functionality and user experience.

--- his-afp/src/app/card-pz/card-pz.ts
+++ his-afp/src/app/card-pz/card-pz.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { Button } from 'primeng/button';
 import { Paziente } from '../models/Paziente.model';
@@ -11,17 +11,20 @@ import { Paziente } from '../models/Paziente.model';
 })
 export class CardPz {
   nome: string = 'Pietro';
-  paziente = signal<Paziente>({
-    braccialetto: 'PZ254',
-    codiceColore: 'ROSSO',
-    cognome: 'Rocchio',
-    eta: 25,
-    id: '23',
-    nome: 'Pietro',
-    note: "Difficoltà respiratoria con dolore mandibolare irradiato all'arto sup. sx. di 9",
-    patologia: 'C19',
-    oraArrivo: new Date(Date.now() - 7200000).toISOString(),
-  });
+
+  paziente = input.required<Paziente>();
+
+  // paziente = signal<Paziente>({
+  //   braccialetto: 'PZ254',
+  //   codiceColore: 'ROSSO',
+  //   cognome: 'Rocchio',
+  //   eta: 25,
+  //   id: '23',
+  //   nome: 'Pietro',
+  //   note: "Difficoltà respiratoria con dolore mandibolare irradiato all'arto sup. sx. di 9",
+  //   patologia: 'C19',
+  //   oraArrivo: new Date(Date.now() - 7200000).toISOString(),
+  // });
 
   cambiaNome() {
     this.nome = 'Gian';
--- his-afp/src/app/elenco-pz/elenco-pz.ts
+++ his-afp/src/app/elenco-pz/elenco-pz.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
-import { CardPz } from '../card-pz/card-pz';
 import { Paziente } from '../models/Paziente.model';
+import { CardPz } from '../card-pz/card-pz';
 
 @Component({
   selector: 'his-elenco-pz',
@@ -9,5 +9,40 @@ import { Paziente } from '../models/Paziente.model';
   styleUrl: './elenco-pz.scss',
 })
 export class ElencoPz {
-  listaPz = signal<Paziente[]>([]);
+  listaPz = signal<Paziente[]>([
+    {
+      braccialetto: 'PZ254',
+      codiceColore: 'ROSSO',
+      cognome: 'Rocchio',
+      eta: 25,
+      id: '23',
+      nome: 'Pietro',
+      note: "Difficoltà respiratoria con dolore mandibolare irradiato all'arto sup. sx. di 9",
+      patologia: 'C19',
+      oraArrivo: new Date(Date.now() - 7200000).toISOString(),
+    },
+    {
+      braccialetto: 'DF090',
+      codiceColore: 'VERDE',
+      cognome: 'Brazorf',
+      eta: 25,
+      id: '9',
+      nome: 'Ajeje',
+      note: 'Non ha il biglietto del bus e non sa dove si trova',
+      patologia: 'C19',
+      oraArrivo: new Date(Date.now() - 7200000).toISOString(),
+    },
+    {
+      braccialetto: 'NB342',
+      codiceColore: 'ARANCIONE',
+      cognome: 'Winky',
+      eta: 25,
+      id: '8',
+      nome: 'Tinky',
+      note: 'Dolore addominale diffuso da 3 giorni con vomito e febbre alta',
+      patologia: 'C01',
+      oraArrivo: new Date(Date.now() - 7200000).toISOString(),
+    },
+  ]);
+  listaNomi = signal<string[]>(['Pietro', 'Gian', 'Lucio']);
 }
