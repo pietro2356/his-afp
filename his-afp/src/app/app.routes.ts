0b829fc0d7f5b7e7d8e317d287c3e95bc00a8cfe
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-02 12:55:50 +0100

    feat: ✨ add new routes for lista-pz, accettazione-pz, modifica-pz, and stato-servizi
    This commit introduces new routes to the application, allowing navigation to the components lista-pz, accettazione-pz, modifica-pz, and stato-servizi. Additionally, a wildcard route is added to redirect any unmatched paths to lista-pz, ensuring a smoother user experience.

--- his-afp/src/app/app.routes.ts
+++ his-afp/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'lista-pz',
+    loadComponent: () => import('./features/lista-pz/lista-pz').then((m) => m.ListaPz),
+  },
+  {
+    path: 'accettazione-pz',
+    loadComponent: () =>
+      import('./features/accettazione-pz/accettazione-pz').then((m) => m.AccettazionePz),
+  },
+  {
+    path: 'modifica-pz',
+    loadComponent: () => import('./features/modifica-pz/modifica-pz').then((m) => m.ModificaPz),
+  },
+  {
+    path: 'stato-servizi',
+    loadComponent: () =>
+      import('./features/stato-servizi/stato-servizi').then((m) => m.StatoServizi),
+  },
+  {
+    path: '**',
+    redirectTo: 'lista-pz',
+    pathMatch: 'full',
+  },
+];
