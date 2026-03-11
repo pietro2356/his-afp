ce574273a7659bda4be3d17718dcbd4652218584
YRNMAMA <mamadou.gningue@marconirovereto.it>
2026-03-11 15:45:45 +0100

    fix codice ed es 1

--- his-afp/src/app/features/stato-servizi/stato-servizi.ts
+++ his-afp/src/app/features/stato-servizi/stato-servizi.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { StatoAPI } from "../../ui/stato-api/stato-api";
 
 @Component({
   selector: 'his-stato-servizi',
-  imports: [],
+  imports: [StatoAPI],
   templateUrl: './stato-servizi.html',
   styleUrl: './stato-servizi.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class StatoServizi {
 
 }
