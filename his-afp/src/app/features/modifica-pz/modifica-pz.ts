af1394b227910b1598a8356af9f6c18337083ef5
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-12 11:02:23 +0100

    feat: ✨ update routing for modifica-pz and enhance patient loading
    Refactor the routing for the modifica-pz feature to use a dynamic patient ID in the URL.
    Update the navigation method to construct the URL with the patient ID directly.
    Enhance the modifica-pz component to handle patient data loading with improved error handling
    and loading states, providing a better user experience.

--- his-afp/src/app/features/modifica-pz/modifica-pz.ts
+++ his-afp/src/app/features/modifica-pz/modifica-pz.ts
@@ -1,12 +1,31 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { PazienteDTO } from '../../core/Pazienti/Pazienti.model';
+import { APIResponse } from '../../core/models/APIResponse.model';
+import { JsonPipe } from '@angular/common';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'his-modifica-pz',
-  imports: [],
+  imports: [JsonPipe, Button],
   templateUrl: './modifica-pz.html',
   styleUrl: './modifica-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModificaPz {
   patientId = input<string>();
+
+  patient = httpResource<APIResponse<PazienteDTO>>(
+    () => `http://localhost:3000/admissions/${this.patientId()}`,
+  );
+
+  constructor() {
+    effect(() => {
+      if (this.patientId() === undefined) {
+        console.warn(
+          'Patient ID is undefined. Please provide a valid patient ID in the route parameters.',
+        );
+      }
+    });
+  }
 }
