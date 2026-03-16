00b1ea6899b48f3ae42d4617c4f37d7d8b1d8d53
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-16 22:06:26 +0100

    feat: ✨ add address fields to patient acceptance form
    Enhance the patient acceptance form by adding a new address section. This includes fields for 'via', 'civico', 'comune', and 'provincia', allowing users to input complete address information. The form is now more comprehensive and user-friendly, improving data collection for patient records.
    
    SCOPE: Creating nested form groups
    REF: https://angular.dev/guide/forms/reactive-forms#creating-nested-form-groups

--- his-afp/src/app/ui/form-accettazione/form-accettazione.ts
+++ his-afp/src/app/ui/form-accettazione/form-accettazione.ts
@@ -3,25 +3,31 @@ import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validator
 import { FlexColP } from '../../core/directive/flex-col-p';
 import { InputTextModule } from 'primeng/inputtext';
 import { Button } from 'primeng/button';
+import { InputNumber } from 'primeng/inputnumber';
 
 interface IFormAccettazione {
   nome: AbstractControl;
   cognome: AbstractControl;
+  address: AbstractControl;
 }
 
 @Component({
   selector: 'his-form-accettazione',
-  imports: [FlexColP, ReactiveFormsModule, InputTextModule, Button],
+  imports: [FlexColP, ReactiveFormsModule, InputTextModule, Button, InputNumber],
   templateUrl: './form-accettazione.html',
   styleUrl: './form-accettazione.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormAccettazione {
-  //nome = new FormControl('');
-
   paziente = new FormGroup<IFormAccettazione>({
     nome: new FormControl('', Validators.required),
     cognome: new FormControl('', Validators.required),
+    address: new FormGroup({
+      via: new FormControl(''),
+      civico: new FormControl(0),
+      comune: new FormControl(''),
+      provincia: new FormControl(''),
+    }),
   });
 
   submitForm() {
