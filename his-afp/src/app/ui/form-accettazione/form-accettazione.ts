075e2493ba96e9b757bc5e4ef04ddbafb3f020d2
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-16 21:51:58 +0100

    feat: ✨ implement reactive form for patient acceptance
    Refactor the form-accettazione component to utilize a reactive form approach. Introduced a FormGroup for managing form controls for 'nome' and 'cognome'. Updated the template to bind the form controls correctly and added a submit handler to log the form data upon submission. This enhances form validation and improves user interaction.
    
    SCOPE: Grouping form controls
    REF: https://angular.dev/guide/forms/reactive-forms#grouping-form-controls

--- his-afp/src/app/ui/form-accettazione/form-accettazione.ts
+++ his-afp/src/app/ui/form-accettazione/form-accettazione.ts
@@ -1,15 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { FlexColP } from '../../core/directive/flex-col-p';
 import { InputTextModule } from 'primeng/inputtext';
+import { Button } from 'primeng/button';
+
+interface IFormAccettazione {
+  nome: AbstractControl;
+  cognome: AbstractControl;
+}
 
 @Component({
   selector: 'his-form-accettazione',
-  imports: [FlexColP, ReactiveFormsModule, InputTextModule],
+  imports: [FlexColP, ReactiveFormsModule, InputTextModule, Button],
   templateUrl: './form-accettazione.html',
   styleUrl: './form-accettazione.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormAccettazione {
-  nome = new FormControl('');
+  //nome = new FormControl('');
+
+  paziente = new FormGroup<IFormAccettazione>({
+    nome: new FormControl(''),
+    cognome: new FormControl(''),
+  });
+
+  submitForm() {
+    console.info('Form inviata!', this.paziente.value);
+  }
 }
