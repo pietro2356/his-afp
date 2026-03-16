88758d3801ff5b5ed95ad0ad1d0929ad0735a11d
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-16 23:27:08 +0100

    feat: ✨ add validation and error messages to form fields
    Enhance the patient acceptance form by adding validation rules and error messages for required fields. Each input field now checks for validity and displays an error message if the field is empty upon submission or when interacted with. This improves user experience by providing immediate feedback on form completion requirements.

--- his-afp/src/app/ui/form-accettazione/form-accettazione.ts
+++ his-afp/src/app/ui/form-accettazione/form-accettazione.ts
@@ -1,38 +1,41 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { AbstractControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FlexColP } from '../../core/directive/flex-col-p';
 import { InputTextModule } from 'primeng/inputtext';
 import { Button } from 'primeng/button';
 import { InputNumber } from 'primeng/inputnumber';
-
-interface IFormAccettazione {
-  nome: AbstractControl;
-  cognome: AbstractControl;
-  address: AbstractControl;
-}
+import { Message } from 'primeng/message';
 
 @Component({
   selector: 'his-form-accettazione',
-  imports: [FlexColP, ReactiveFormsModule, InputTextModule, Button, InputNumber],
+  imports: [FlexColP, ReactiveFormsModule, InputTextModule, Button, InputNumber, Message],
   templateUrl: './form-accettazione.html',
   styleUrl: './form-accettazione.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormAccettazione {
+  isSubmitted = signal<boolean>(false);
   readonly #formBuilder = inject(FormBuilder);
-
   paziente = this.#formBuilder.group({
-    nome: [''],
-    cognome: [''],
+    nome: ['', Validators.required, Validators.minLength(3), Validators.maxLength(30)],
+    cognome: ['', Validators.required, Validators.minLength(3), Validators.maxLength(30)],
     address: this.#formBuilder.group({
-      via: [''],
-      civico: [''],
-      comune: [''],
-      provincia: [''],
+      via: ['', Validators.required, Validators.minLength(3), Validators.maxLength(30)],
+      civico: ['', Validators.required, Validators.min(0)],
+      comune: ['', Validators.required, Validators.minLength(3), Validators.maxLength(30)],
+      provincia: ['', Validators.required, Validators.minLength(2), Validators.maxLength(2)],
     }),
   });
 
+  checkFCValidity(fc: string): boolean {
+    const fcStatus = this.paziente.get(fc);
+    if (!fcStatus) return false;
+
+    return fcStatus.invalid && (fcStatus.dirty || fcStatus.touched || this.isSubmitted());
+  }
+
   submitForm() {
     console.info('Form inviata!', this.paziente.value);
+    this.isSubmitted.set(true);
   }
 }
