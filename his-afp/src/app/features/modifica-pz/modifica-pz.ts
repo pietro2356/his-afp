68d6c2c67a082cbb7864b59ac139032095af5dee
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-25 23:51:03 +0100

    feat: ✨ add sanitaria section with validation for patient data
    Implement a new section in the patient modification form to capture sanitaria details, including required fields for patologia, codice colore, modalità di arrivo, and note triage. Each field includes validation to ensure data integrity, enhancing the overall functionality of the form.

--- his-afp/src/app/features/modifica-pz/modifica-pz.ts
+++ his-afp/src/app/features/modifica-pz/modifica-pz.ts
@@ -1,30 +1,42 @@
-import { ChangeDetectionStrategy, Component, effect, inject, input } from '@angular/core';
-import { httpResource } from '@angular/common/http';
-import { PazienteDTO } from '../../core/Pazienti/Pazienti.model';
-import { APIResponse } from '../../core/models/APIResponse.model';
-import { formatDate } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { DatePicker } from 'primeng/datepicker';
-import { Fieldset } from 'primeng/fieldset';
-import { InputText } from 'primeng/inputtext';
-import { Message } from 'primeng/message';
-import { Select } from 'primeng/select';
+import {ChangeDetectionStrategy, Component, effect, inject, input, signal} from '@angular/core';
+import {httpResource} from '@angular/common/http';
+import {PazienteDTO} from '../../core/Pazienti/Pazienti.model';
+import {APIResponse} from '../../core/models/APIResponse.model';
+import {formatDate} from '@angular/common';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {DatePicker} from 'primeng/datepicker';
+import {Fieldset} from 'primeng/fieldset';
+import {InputText} from 'primeng/inputtext';
+import {Message} from 'primeng/message';
+import {Select} from 'primeng/select';
+import {Textarea} from 'primeng/textarea';
+import {GestioneRisorse} from '../../core/Risorse/gestione-risorse';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'his-modifica-pz',
-  imports: [DatePicker, Fieldset, InputText, Message, ReactiveFormsModule, Select],
+  imports: [
+    DatePicker,
+    Fieldset,
+    InputText,
+    Message,
+    ReactiveFormsModule,
+    Select,
+    Textarea,
+    Button,
+  ],
   templateUrl: './modifica-pz.html',
   styleUrl: './modifica-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModificaPz {
   readonly fb = inject(FormBuilder);
+  readonly gestioneRisorse = inject(GestioneRisorse);
   patientId = input<string>();
-
+  canEditAnagrafica = signal<boolean>(false);
   patient = httpResource<APIResponse<PazienteDTO>>(
     () => `http://localhost:3000/admissions/${this.patientId()}`,
   );
-
   // All'interno del componente
   readonly admissionForm = this.fb.group({
     anagrafica: this.fb.group({
@@ -36,6 +48,12 @@ export class ModificaPz {
         [Validators.required, Validators.pattern(/^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/i)],
       ],
       sesso: ['', [Validators.required]],
+    }),
+    sanitaria: this.fb.group({
+      patologia: ['', [Validators.required]],
+      codiceColore: ['', [Validators.required]],
+      modArrivo: ['', [Validators.required]],
+      noteTriage: ['', [Validators.required, Validators.maxLength(500)]],
     }),
   });
 
@@ -52,9 +70,24 @@ export class ModificaPz {
             codiceFiscale: data?.codiceFiscale ?? '',
             sesso: data?.sex ?? '',
           },
+          sanitaria: {
+            patologia: data?.patologiaCode ?? '',
+            codiceColore: data?.coloreCode ?? '',
+            modArrivo: data?.modalitaArrivoCode ?? '',
+            noteTriage: data?.noteTriage ?? '',
+          },
         });
+        this.admissionForm.get('anagrafica')?.disable();
       }
     });
+  }
+
+  toggleEditAnagrafica() {
+    this.canEditAnagrafica()
+      ? this.admissionForm.get('anagrafica')?.disable()
+      : this.admissionForm.get('anagrafica')?.enable();
+
+    this.canEditAnagrafica.set(!this.canEditAnagrafica());
   }
 
   checkFormFieldValidity(field: string) {
