5f698479f8c75fa82eed1bed8cd2c90417a00b30
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-29 22:55:53 +0200

    feat: ✨ implement patient table with dynamic data display
    Replace the previous patient list structure with a responsive table component that displays patient details dynamically. The new implementation includes headers for ID, color code, personal information, bracelet number, age, pathology, and notes, enhancing the overall layout and usability of the patient data presentation.

--- his-afp/src/app/pattern/tabella-pz/tabella-pz.ts
+++ his-afp/src/app/pattern/tabella-pz/tabella-pz.ts
@@ -1,32 +1,15 @@
-import { ChangeDetectionStrategy, Component, effect, inject, model } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PatientManager } from '../../core/Pazienti/patient-manager';
 import { FormsModule } from '@angular/forms';
-import { InputText } from 'primeng/inputtext';
-import { CardPz } from '../../ui/card-pz/card-pz';
-import { Button } from 'primeng/button';
-import { ToggleSwitch } from 'primeng/toggleswitch';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'his-tabella-pz',
-  imports: [FormsModule, InputText, CardPz, Button, ToggleSwitch],
+  imports: [FormsModule, TableModule],
   templateUrl: './tabella-pz.html',
   styleUrl: './tabella-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabellaPz {
   readonly PatientManager = inject(PatientManager);
-  nomePaziente = model<string>('');
-  enableRefreshPz = model<boolean>(false);
-
-  constructor() {
-    effect(() => {
-      this.PatientManager.filterByName(this.nomePaziente());
-
-      if (this.enableRefreshPz()) {
-        this.PatientManager.refreshPazienti();
-      } else {
-        this.PatientManager.stopRefreshPazienti();
-      }
-    });
-  }
 }
