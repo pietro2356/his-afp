8a4fdb26bcea479fa6684cf7a9d46e2e1bac1eea
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-31 01:04:28 +0200

    feat: 🎉 add nginx configuration for frontend and update version to v2.0.0
    Introduce a new nginx configuration file to manage the frontend server routing. The configuration sets up an upstream for the frontend-green server and defines proxy settings for both the main application and API requests. Additionally, update the version in env.template.js to v2.0.0 to reflect the latest changes.

--- his-afp/public/env.template.js
+++ his-afp/public/env.template.js
@@ -3,5 +3,5 @@
   window['ver'] = window['ver'] || {};
   // Questo segnaposto verrà sostituito all'avvio del container
   window['env']['type'] = '${APP_ENV}';
-  window['ver']['type'] = 'v1.2.1';
+  window['ver']['type'] = 'v2.0.0';
 })(this);
