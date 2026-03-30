b4a52edd90c010cec518a7c6533c864c7e63b064
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-31 00:08:48 +0200

    feat: 🔧 fix version assignment in env.template.js
    correct the version assignment to ensure it initializes properly. the previous implementation was incorrectly assigning the version to the env object instead of the ver object.

--- his-afp/public/env.template.js
+++ his-afp/public/env.template.js
@@ -1,6 +1,6 @@
 (function (window) {
   window['env'] = window['env'] || {};
-  window['ver'] = window['env'] || {};
+  window['ver'] = window['ver'] || {};
   // Questo segnaposto verrà sostituito all'avvio del container
   window['env']['type'] = '${APP_ENV}';
   window['ver']['type'] = 'v1.0.5';
