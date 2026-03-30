290536e0ef0929eaa0cc74bede8c72e26c8cb5e6
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-31 00:29:48 +0200

    feat: 🔧 update version in env.template.js to v1.1.2
    Bump the version number in env.template.js from v1.1.1 to v1.1.2 to reflect the latest changes in the application environment.

--- his-afp/public/env.template.js
+++ his-afp/public/env.template.js
@@ -3,5 +3,5 @@
   window['ver'] = window['ver'] || {};
   // Questo segnaposto verrà sostituito all'avvio del container
   window['env']['type'] = '${APP_ENV}';
-  window['ver']['type'] = 'v1.1.1';
+  window['ver']['type'] = 'v1.1.2';
 })(this);
