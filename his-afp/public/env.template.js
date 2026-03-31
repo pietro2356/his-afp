e885bd498c3d3061159ba014734db5eb2d4ec6be
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-31 11:20:03 +0200

    feat: 🔧 update version in env.template.js to v2.0.1
    This commit updates the version number in the env.template.js file from v2.0.0 to v2.0.1. This change ensures that the application reflects the latest version in its environment configuration.

--- his-afp/public/env.template.js
+++ his-afp/public/env.template.js
@@ -3,5 +3,5 @@
   window['ver'] = window['ver'] || {};
   // Questo segnaposto verrà sostituito all'avvio del container
   window['env']['type'] = '${APP_ENV}';
-  window['ver']['type'] = 'v2.0.0';
+  window['ver']['type'] = 'v2.0.1';
 })(this);
