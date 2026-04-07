8363caa2216b2efffeed394679e013ff115e0e76
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-04-07 12:46:25 +0200

    feat: ✨ add is_active field to user table
    Add a new boolean field 'is_active' to the user table to track the active status of users. This field defaults to TRUE, ensuring that new users are active upon creation.

--- db/init.sql
+++ db/init.sql
@@ -66,7 +66,8 @@ CREATE TABLE IF NOT EXISTS users
     username   VARCHAR(50) UNIQUE NOT NULL,
     password   VARCHAR(255)       NOT NULL,
     role       user_role          NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP                   DEFAULT CURRENT_TIMESTAMP,
+    is_active  BOOLEAN            NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE IF NOT EXISTS patients
