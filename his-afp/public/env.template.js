9c0cc5fe9db0f31d367dbfcb32427a5cf6c2ed51
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-04-08 15:50:53 +0200

    feat: 🎉 add environment configuration files and update frontend to load env variables
    Added new JSON configuration files for different environments (test, production, and form) to manage environment-specific settings. Updated the frontend to load these environment variables dynamically from the respective files. Modified the Dockerfile to clean up old environment files and adjusted the docker-compose configuration to mount the environments directory. Enhanced the frontend component to display environment properties and handle loading errors gracefully.

--- his-afp/public/env.template.js
+++ his-afp/public/env.template.js
@@ -1,7 +1,5 @@
 (function (window) {
   window['env'] = window['env'] || {};
-  window['ver'] = window['ver'] || {};
   // Questo segnaposto verrà sostituito all'avvio del container
   window['env']['type'] = '${APP_ENV}';
-  window['ver']['type'] = 'v2.0.1';
 })(this);
--- his-afp/src/app/features/stato-servizi/stato-servizi.ts
+++ his-afp/src/app/features/stato-servizi/stato-servizi.ts
@@ -1,12 +1,46 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Button } from 'primeng/button';
+
+interface IEnvProp {
+  production: boolean;
+  environment: string;
+  version: string;
+  apiUrl: string;
+}
+
+const ENV_PROP_MOCK: IEnvProp = {
+  production: false,
+  environment: 'MOCK',
+  version: 'N/A',
+  apiUrl: 'N/A',
+};
 
 @Component({
   selector: 'his-stato-servizi',
-  imports: [],
+  imports: [Button],
   templateUrl: './stato-servizi.html',
   styleUrl: './stato-servizi.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatoServizi {
+  readonly prop = signal<IEnvProp>(ENV_PROP_MOCK);
+  env = (window as any).env?.type || 'unknown';
+  readonly #http = inject(HttpClient);
 
+  loadEnvPropViaFile() {
+    if (this.env === 'unknown') {
+      console.warn('Environment type is unknown. Cannot load environment properties.');
+      return;
+    }
+    this.#http.get<IEnvProp>(`./env/${this.env}.json`).subscribe({
+      next: (data) => {
+        console.table(data);
+        this.prop.set(data);
+      },
+      error: (err) => {
+        console.error('Error loading environment properties:', err);
+      },
+    });
+  }
 }
