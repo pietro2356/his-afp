b5eb44ebfd8b73897e89763a8751df00ea542ff9
LucaBaldessari <luca.baldessari@mat.tn.it>
2026-07-24 09:27:53 +0000

    git commit -m "feat: aggiunta gestione staff con UserService, AsyncValidator e GestioneStaffComponent"

--- his-afp/src/app/app.routes.ts
+++ his-afp/src/app/app.routes.ts
@@ -10,12 +10,7 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/accettazione-pz/accettazione-pz').then((m) => m.AccettazionePz),
   },
-  // {
-  //   path: 'modifica-pz',
-  //   loadComponent: () => import('./features/modifica-pz/modifica-pz').then((m) => m.ModificaPz),
-  // },
   {
-    // /modifica-pz?id=2
     path: 'modifica-pz/:patientId',
     loadComponent: () => import('./features/modifica-pz/modifica-pz').then((m) => m.ModificaPz),
   },
@@ -23,6 +18,11 @@ export const routes: Routes = [
     path: 'stato-servizi',
     loadComponent: () =>
       import('./features/stato-servizi/stato-servizi').then((m) => m.StatoServizi),
+  },
+  {
+    path: 'staff',
+    loadComponent: () =>
+      import('./features/gestione-staff/gestione-staff').then((m) => m.GestioneStaffComponent),
   },
   {
     path: '',
---
+++ his-afp/src/app/features/gestione-staff/gestione-staff.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../core/services/user.service';
+import { usernameAvailableValidator } from '../../core/validators/username.validator';
+import { UserRole } from '../../core/models/user.model';
+
+@Component({
+  selector: 'app-gestione-staff',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './gestione-staff.html',
+  styleUrls: ['./gestione-staff.scss']
+})
+export class GestioneStaffComponent implements OnInit {
+  public userService = inject(UserService);
+  private fb = inject(FormBuilder);
+
+  staffForm!: FormGroup;
+  roles: UserRole[] = ['DOC', 'INF', 'AMM'];
+  successMessage: string | null = null;
+
+  ngOnInit(): void {
+    this.userService.loadUsers();
+
+    this.staffForm = this.fb.group({
+      username: [
+        '',
+        [Validators.required, Validators.minLength(3)],
+        [usernameAvailableValidator(this.userService)]
+      ],
+      role: ['DOC' as UserRole, [Validators.required]]
+    });
+  }
+
+  get usernameControl() {
+    return this.staffForm.get('username');
+  }
+
+  onSubmit(): void {
+    if (this.staffForm.invalid) {
+      this.staffForm.markAllAsTouched();
+      return;
+    }
+
+    this.userService.createUser(this.staffForm.value).subscribe({
+      next: () => {
+        this.successMessage = 'Utente creato con successo!';
+        this.staffForm.reset({ role: 'DOC' });
+        setTimeout(() => (this.successMessage = null), 4000);
+      }
+    });
+  }
+
+  onRoleChange(userId: number, event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const newRole = selectElement.value as UserRole;
+
+    this.userService.updateRole(userId, newRole).subscribe();
+  }
+}
